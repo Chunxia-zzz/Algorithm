209414200658286997d37b14ddb9f1777f20216e
Chunxia <1317214963@qq.com>
2022-06-01 11:22:19 +0800

    add coding

--- 2、python数据结构/string_method/切片.py
+++ 2、python数据结构/string_method/切片.py
@@ -7,6 +7,7 @@ abABCc
 
 切前n位[0:n]
 后len(str)-n位 [n:0]
+
 切后n位[len(str)-n:0]
 剩余
 
@@ -14,12 +15,18 @@ print(str1[0:n])
 截取字符串，使用思路切片
 
 '''
-while True:
-    try:
-        s=input()           
-        while len(s)>8:               
-            print(s[:8])               
-            s=s[8:]           
-        print(s.ljust(8,"0"))
-    except:
-        break
+# while True:
+#     try:
+#         s=input()           
+#         while len(s)>8:               
+#             print(s[:8])               
+#             s=s[8:]           
+#         print(s.ljust(8,"0"))
+#     except:
+#         break
+
+a = 200
+b = bin(a)
+c =b[2:].split('0')
+print(c)
+print(len(max(c)))

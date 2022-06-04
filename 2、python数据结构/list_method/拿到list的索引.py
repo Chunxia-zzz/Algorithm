ab9bdd46420a639c82168984ac9041da1b0f6791
Chunxia <1317214963@qq.com>
2022-06-04 23:23:29 +0800

    update 0604

--- 2、python数据结构/list_method/拿到list的索引.py
+++ 2、python数据结构/list_method/拿到list的索引.py
@@ -1,3 +1,6 @@
+"""
+索引，索引取值
+"""
 list1 = [1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 1, 1]
 max_value  = max(list1)
 idx = list1.index(max_value)
@@ -5,3 +8,7 @@ print(idx)
 
 x = list(enumerate(list1))
 print(x)
+
+list2 = [0,1,2]
+#-1表示最后一位，-2表示倒数第二位。依此类推
+print(list2[-1])
--- demo.py
+++ demo.py
@@ -1,31 +1,30 @@
 """
 """
-# import time
+import time
 
-# class LogTime:
-#     # def __init__(self, use_int):
-#     #     self.use_int = use_int
-#     def __call__(self, func):
-#         def _log():
-#             beg = time.time()
-#             res = func()
-#             print('use time: {}'.format(time.time()-beg))
-#             # if self.use_int:
-#             #     print('use time: {}'.format(time.time()-beg))
-#             # else:
-#             #     print('use time: {}'.format(time.time()-beg))
-#             return res
-#         return _log 
+class LogTime:
+    # def __init__(self, use_int):
+    #     self.use_int = use_int
+    def __call__(self, func):
+        def _log():
+            beg = time.time()
+            res = func()
+            print('use time: {}'.format(time.time()-beg))
+            # if self.use_int:
+            #     print('use time: {}'.format(time.time()-beg))
+            # else:
+            #     print('use time: {}'.format(time.time()-beg))
+            return res
+        return _log 
 
-# @LogTime()
-# def mysleep2():
-#     time.sleep(2)
+@LogTime()
+def mysleep2():
+    time.sleep(2)
 
-# mysleep2()
+mysleep2()
 # for i in range(5):
 #     print(i)
-stack = [0,1,2]
-print( stack[-1])
+
 
 
 
